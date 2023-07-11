5b69b160f1c9eb0ac60d73c3f0207ecf151132df
mucarb <murilo.rb60@gmail.com>
2023-07-11 16:35:29 -0300

    Project created

---
+++ src/main/java/com/murilo/dslistapi/DslistapiApplication.java
@@ -0,0 +1,13 @@
+package com.murilo.dslistapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DslistapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DslistapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/murilo/dslistapi/DslistapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.murilo.dslistapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslistapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
